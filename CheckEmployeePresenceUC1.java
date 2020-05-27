419478ef06be6bfb98a12d95d4ae31fddab7ed89
karthik <nallaboluk@gmail.com>
2020-05-27 07:30:12 +0530

    adding CheckEmployeePresenceUC1

---
+++ CheckEmployeePresenceUC1.java
@@ -0,0 +1,13 @@
+
+import java.util.*; 
+public class CheckEmployeePresenceUC1 {
+
+	public static void main(String[] args) {
+		int isPresent = 1;
+		double empCheck = Math.floor(Math.random() * 10)%2;         
+		if (empCheck == isPresent)
+			System.out.println("employee is present");
+		else
+			System.out.println("employee is absent");
+		}
+}
