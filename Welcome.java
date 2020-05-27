bb8416a7ea1da083a22b3133be70f2fc56c6bc8d
karthik <nallaboluk@gmail.com>
2020-05-27 07:15:45 +0530

    adding welcome program

---
+++ Welcome.java
@@ -0,0 +1,10 @@
+import java.util.*; 
+public class Welcome {
+
+	public static void main(String[] args) {
+		
+		System.out.println("welcome to employee wage computation problem on master branch");
+		}
+}
+
+
